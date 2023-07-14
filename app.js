abfab00771f7b58fe1bf07c2f0164b96b2af5895
David Peter <davidpeter.di@gmail.com>
2023-07-14 22:34:45 +0100

    Local Application

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV !== "production") {
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = 'mongodb://localhost:27017/yelp-camp';
 mongoose.connect(dbUrl);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Connection error'));
