dacf7fdd1373e4a22c6c5383baa7b1a6108a9e88
David Peter <davidpeter.di@gmail.com>
2023-09-13 12:48:22 +0100

    Updating DB Connection String

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV !== "production") {
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/YelpCamp';
 mongoose.connect(dbUrl);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Connection error'));
